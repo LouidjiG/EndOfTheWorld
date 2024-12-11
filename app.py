c6020e3ae498f98855f92fa0c980747ccd1addf9
LouidjiG <louidji.graveleau@edu.esiee.fr>
2024-12-11 11:39:56 +0100

    Modify the hero section with animation

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+
+@app.route('/wallpapers/<path:filename>')
+def wallpapers(filename):
+    return send_from_directory('/user/gravelel/Téléchargements/EndOfTheWorld/Wallpapers', filename)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
